bfd7df002fefaeb3cde4b6830f974376e41869fe
Dino <dinokunovac@gmail.com>
2020-03-29 18:17:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 // Code your solution in this file
-function findMatching (list, name) {
-    return list.filter(function (driverName) {
-      return driverName.toLowerCase() === name.toLowerCase();
-    });
-  }
+// function findMatching (list, name) {
+//     return list.filter(function (driverName) {
+//       return driverName.toLowerCase() === name.toLowerCase();
+//     });
+//   }
 
-  function fuzzyMatch (list, partialName) {
-    let lengthOfName = partialName.length;
+const findMatching = (list, name) => {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+
+  // function fuzzyMatch (list, partialName) {
+  //   let lengthOfName = partialName.length;
+  //   return list.filter(function (driverName) {
+  //     return driverName.slice(0, lengthOfName) === partialName;
+  //   });
+  // }
+
+  const fuzzyMatch = (list, partialName) => {
+    let lengthOfName = partialName.length
     return list.filter(function (driverName) {
-      return driverName.slice(0, lengthOfName) === partialName;
-    });
+      return driverName.slice(0, lengthOfName) === partialName
+    })
   }
   
-  function matchName (list, name) {
+  // function matchName (list, name) {
+  //   return list.filter(function (driver) {
+  //     return driver.name.toLowerCase() === name.toLowerCase();
+  //   });
+  // }
+  const matchName = (list, name) => {
     return list.filter(function (driver) {
-      return driver.name.toLowerCase() === name.toLowerCase();
-    });
+      return driver.name.toLowerCase() === name.toLowerCase()
+    })
   }
